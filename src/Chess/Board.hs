771187ae158b9f32f256e08ab14661aeb0712948
mjachi <mmmeeeker@gmail.com>
2022-08-31 05:21:33 -0400

    Board abstraction, moves next

--- src/Chess/Board.hs
+++ src/Chess/Board.hs
@@ -7,13 +7,13 @@ import Control.DeepSeq
 import qualified Data.List as List (intersect)
 import Data.Matrix
 import GHC.Generics
+import Player (Color (Black, White))
 
-type Board = Matrix Square
+-----------------------------------------------------------------------------------------
+-- BOARD TYPE DEFS
+-----------------------------------------------------------------------------------------
 
-type Position = (Int, Int)
-
-data Square = BlankSquare | OccupiedSquare
-
+-- | Chess peices include Pawn, Knight, Bishop, Rook, Queen, King.
 data Piece = Pawn | Knight | Bishop | Rook | Queen | King
   deriving (Eq, Generic, NFData)
 
@@ -25,3 +25,58 @@ instance Show Piece where
     Rook -> "R"
     Queen -> "Q"
     King -> "K"
+
+-- | Colored Piece. Associates a piece to a color, meaning a player assumes ownership/ control thereof.
+newtype CPiece = CPiece (Color, Piece) deriving (Eq, Generic)
+
+instance Show CPiece where
+  show (CPiece (color, piece)) = show color ++ show piece
+
+-- | A position is by the piece's coordinates
+type Position = (Int, Int)
+
+-- | Square in the Board Matrix is either occupied or black
+data Square = BlankSquare | OccupiedSquare CPiece
+
+instance Show Square where
+  show BlankSquare = "_"
+  show (OccupiedSquare cp) = show cp
+
+-- | A chessboard is to be defined as an 8x8 matrix
+type Board = Matrix Square
+
+-- | Construct a new, blank chessboard
+newBoard :: (Position -> Square) -> Board
+newBoard = matrix 8 8
+
+-- | Constructs a board from provided arguments.
+newBoardFromList :: [Square] -> Board
+newBoardFromList = fromList 8 8
+
+---------------------------------------------------------------------------------------
+-- BOARD INITS
+---------------------------------------------------------------------------------------
+
+classicBoard :: Board
+classicBoard = newBoard classicLayout
+
+-- | Defines the classic chess layout, coloring pieces, etc.
+classicLayout :: Position -> Square
+classicLayout (i, j)
+  | (i == 1) && (j == 1 || j == 8) = OccupiedSquare $ CPiece (Black, Rook)
+  | (i == 1) && (j == 2 || j == 7) = OccupiedSquare $ CPiece (Black, Knight)
+  | (i == 1) && (j == 3 || j == 6) = OccupiedSquare $ CPiece (Black, Bishop)
+  | (i == 1) && (j == 4) = OccupiedSquare $ CPiece (Black, Queen)
+  | (i == 1) && (j == 5) = OccupiedSquare $ CPiece (Black, King)
+  | i == 2 = OccupiedSquare $ CPiece (Black, Pawn)
+  | (i == 8) && (j == 1 || j == 8) = OccupiedSquare $ CPiece (White, Rook)
+  | (i == 8) && (j == 2 || j == 7) = OccupiedSquare $ CPiece (White, Knight)
+  | (i == 8) && (j == 3 || j == 6) = OccupiedSquare $ CPiece (White, Bishop)
+  | (i == 8) && (j == 4) = OccupiedSquare $ CPiece (White, Queen)
+  | (i == 8) && (j == 5) = OccupiedSquare $ CPiece (White, King)
+  | i == 7 = OccupiedSquare $ CPiece (White, Pawn)
+  | otherwise = BlankSquare
+
+-----------------------------------------------------------------------------------------
+-- BOARD FUNCTIONALITY
+-----------------------------------------------------------------------------------------
--- src/Game.hs
+++ src/Game.hs
@@ -1,22 +1,33 @@
 {-# LANGUAGE DeriveGeneric #-}
 
-
-
 module Game where
 
 import Chess.Board
-import Control.DeepSeq
 import GHC.Generics (Generic)
+import Player (Color (Black, White), Player, colorSwitch)
 
-import Player
+-----------------------------------------------------------------------------------------
+-- GAME DEFINITIONS
+-----------------------------------------------------------------------------------------
 
-
-
-
+-- | Type definition for Game abstraction
 data Game = Game
-  { player1 :: Player.Player,
-    player2 :: Player.Player,
+  { player1 :: Player,
+    player2 :: Player,
     plays :: [Chess.Board.Board],
     turn :: Player.Color
-  } deriving (Generic)
+  }
+  deriving (Generic)
 
+-- | Initializes a blank Game
+newGame :: Player.Player -> Player.Player -> Game
+newGame p1 p2 = new' p1 p2 [] White
+
+-- | Given a new board, progresses the game state by one move.
+updateGame :: Game -> Chess.Board.Board -> Game
+updateGame game newBoard =
+  let p1 = player1 game
+      p2 = player2 game
+      newPlays = newBoard : plays game
+      nextTurn = colorSwitch $ turn game
+   in newGame' p1 p2 newPlays nextTurn
