3245765da3eafe302e032da700c509f31183d1f0
mjachi <mmmeeeker@gmail.com>
2022-09-05 15:48:27 -0400

    Finished chess functionalities

--- src/Chess/Board.hs
+++ src/Chess/Board.hs
@@ -9,10 +9,9 @@ module Chess.Board where
 -----------------------------------------------------------------------------------------
 
 import Control.DeepSeq
-import qualified Data.List as List (intersect)
 import Data.Matrix
 import GHC.Generics
-import Player (Color (Black, White))
+import Player (Color (Black, White), colorSwitch)
 
 -----------------------------------------------------------------------------------------
 -- BOARD TYPE DEFS
@@ -36,13 +35,14 @@ instance Show Piece where
 newtype CPiece = CPiece (Color, Piece) deriving (Eq, Generic)
 
 instance Show CPiece where
-  show (CPiece (color, piece)) = show color ++ show piece
+  show (CPiece (cc, pp)) = show cc ++ show pp
 
 -- | A position is by the piece's coordinates
 type Position = (Int, Int)
 
 -- | Square in the Board Matrix is either occupied or black
 data Square = BlankSquare | OccupiedSquare CPiece
+  deriving (Eq)
 
 instance Show Square where
   show BlankSquare = "_"
@@ -87,12 +87,219 @@ classicLayout (i, j)
 -- BOARD FUNCTIONALITY
 -----------------------------------------------------------------------------------------
 
+legalGrab :: Board -> Color -> Position -> Bool
+legalGrab board turn pos =
+  let sq = uncurry safeGet pos board
+   in case sq of
+        Just BlankSquare -> False
+        Just jsq -> not $ isSquareOccupiedByEnemy jsq turn
+        Nothing -> False
+
+movePiece :: Board -> Color -> Position -> Position -> Maybe Board
+movePiece board turn src dst =
+  let availableMovements = freeMovements board turn src
+   in if dst `elem` availableMovements
+        then Just $ treatPromotions $ placePiece board src dst
+        else Nothing
+
+-- TODO -- generic piece promotion, as opposed to necessitating a Queen
+treatPromotions :: Board -> Board
+treatPromotions board = mapRow (promotePawns White) 1 $ mapRow (promotePawns Black) 8 board
+  where
+    promotePawns cc _ sq =
+      if isSquareOccupiedByPiece cc Pawn sq
+        then OccupiedSquare (CPiece (cc, Queen))
+        else sq
+
 -- | Given a board, starting position, and resulting position, moves piece at A
 --   to B by blanking out A. Does not check validity.
 placePiece :: Board -> Position -> Position -> Board
 placePiece board positionFrom positionTo =
   let srcSquare = board ! positionFrom
    in setElem BlankSquare positionFrom (setElem srcSquare positionTo board)
+
+-- | Returns a list of Positions denoting the grid forming the chessboard as proxy
+-- to the valid Position's on the board
+validChessPositions :: [Position]
+validChessPositions = [(x, y) | x <- [1 .. 8], y <- [1 .. 8]]
+
+-- | Returns a list of postitions for a particular colored piece
+positionsOf :: Board -> Color -> Piece -> [Position]
+positionsOf board cc somePiece =
+  let occupiedPositions = filter (not . isSquareFree board) validChessPositions
+      predicate position = CPiece (cc, somePiece) == piece (square board position)
+   in filter predicate occupiedPositions
+
+enemyMovements :: Board -> Color -> [Position]
+enemyMovements board cc =
+  let enemyColor = colorSwitch cc
+   in concatMap (uncheckedFreeMovements board enemyColor) (ccPositions board enemyColor)
+
+-----------------------------------------------------------------------------------------
+-- GETTERS
+-----------------------------------------------------------------------------------------
+
+-- | Given a board and position, returns the square on that board at that position
+square :: Board -> Position -> Square
+square board pos = board ! pos
+
+-- | Given a square, returns the piece on it
+piece :: Square -> CPiece
+piece (OccupiedSquare colorPiece) = colorPiece
+
+-- | Given a square, returns its color
+color :: Square -> Maybe Color
+color (OccupiedSquare (CPiece (c, _))) = Just c
+color BlankSquare = Nothing
+
+-----------------------------------------------------------------------------------------
+-- STATE CHECKERS
+-----------------------------------------------------------------------------------------
+
+-- | Determines whether or not the current position has ```Color``` ```cc``` in check
+isCheck :: Board -> Color -> Bool
+isCheck board cc = isPositionThreatened board cc $ head $ positionsOf board cc King
+
+-- | Determines whether or not the current position has a ```Position``` ```position```
+-- threatened for ```Color``` ```cc```
+isPositionThreatened :: Board -> Color -> Position -> Bool
+isPositionThreatened board cc position = position `elem` enemyMovements board cc
+
+-- | Given a Board and a Position, determines whether or not the corresponding Square is
+-- empty or not (whether or not it is a BlankSquare)
+isSquareFree :: Board -> Position -> Bool
+isSquareFree board pos = square board pos == BlankSquare
+
+-- | Given a Square and a Color, determines if the piece on that Square is of the
+-- Color argument ```c```
+isSquareOccupiedByColor :: Color -> Square -> Bool
+isSquareOccupiedByColor _ BlankSquare = False
+-- TODO: below definition needs Maybe checking
+isSquareOccupiedByColor c sq = isColor c $ piece sq
+
+-- | Given a Color, Piece, and a Square, determines if the Square is occupied by that
+-- type of Piece of that Color
+isSquareOccupiedByPiece :: Color -> Piece -> Square -> Bool
+isSquareOccupiedByPiece _ _ BlankSquare = False
+isSquareOccupiedByPiece c p (OccupiedSquare (CPiece (cc, pp))) = cc == c && pp == p
+
+-- | Given a Square and a Color, determines if the Square is occupied by a Piece of
+-- opposite Color; if BlankSquare, then false.
+isSquareOccupiedByEnemy :: Square -> Color -> Bool
+isSquareOccupiedByEnemy (OccupiedSquare (CPiece (Black, _))) White = True
+isSquareOccupiedByEnemy (OccupiedSquare (CPiece (White, _))) Black = True
+isSquareOccupiedByEnemy _ _ = False
+
+-- | Given a Color and a CPiece, determines if the given CPiece is of that Color
+isColor :: Color -> CPiece -> Bool
+isColor c (CPiece (pc, _)) = pc == c
+
+-- | Determines if the given Position pos on the Board board is occupied by a King
+isKingOccupied :: Board -> Position -> Bool
+isKingOccupied board pos =
+  isOccupiedBy Black King board pos || isOccupiedBy White King board pos
+
+-- | Determines if the given Position pos on the Board board is occupied by a Knight
+isKnightOccupied :: Board -> Position -> Bool
+isKnightOccupied board pos =
+  isOccupiedBy Black Knight board pos || isOccupiedBy White Knight board pos
+
+-- | Determines if the given Position pos on the Board board is occupied by a Pawn
+isPawnOccupied :: Board -> Position -> Bool
+isPawnOccupied board pos =
+  isOccupiedBy Black Pawn board pos || isOccupiedBy White Pawn board pos
+
+-- | Given a Color cc, Piece pp, Board board, and position pp, determines if pp is
+-- occupied by that CPiece (colored piece)
+isOccupiedBy :: Color -> Piece -> Board -> Position -> Bool
+isOccupiedBy cc pp board pos =
+  let coloredPiece = square board pos
+   in coloredPiece == OccupiedSquare (CPiece (cc, pp))
+
+-- | Determiens if a given Position is in the board (of course, a component of validity)
+isInsideBoard :: Position -> Bool
+isInsideBoard (i, j) = i >= 1 && i <= 8 && j >= 1 && j <= 8
+
+-- | Determines if a given position on a Board is occupied by an enemy piece
+isPositionOccupiedByEnemy :: Board -> Color -> Position -> Bool
+isPositionOccupiedByEnemy board cc position = isSquareOccupiedByEnemy (square board position) cc
+
+-----------------------------------------------------------------------------------------
+-- MOVE FUNCTIONS
+-----------------------------------------------------------------------------------------
+
+allMovements :: CPiece -> Position -> [[Position]]
+allMovements cp pos = map (filter isInsideBoard) $ pieceUnboundedMovements cp pos
+
+availabilityPositionFilter :: Board -> Color -> [Position] -> [Position]
+availabilityPositionFilter board turn positions =
+  let (frees, occupied) = span (isSquareFree board) positions
+   in if (not . null) occupied && isPositionOccupiedByEnemy board turn (head occupied)
+        then head occupied : frees
+        else frees
+
+-- | List of a
+ccPositions :: Board -> Color -> [Position]
+ccPositions board cc = [position | position <- validChessPositions, isSquareOccupiedByColor cc $ square board position]
+
+possibleMovementsForeachTeamPosition :: Board -> Color -> [(Position, [Position])]
+possibleMovementsForeachTeamPosition board cc = map (\src -> (src, freeMovements board cc src)) (ccPositions board cc)
+
+-- | Return list of movements for a given Color given a Board
+ccMovements :: Board -> Color -> [Position]
+ccMovements board cc = concatMap (freeMovements board cc) (ccPositions board cc)
+
+-- | Returns a list of the possible free movements; takes advantage of helper
+-- ```uncheckedFreeMovements``` to this end.
+freeMovements :: Board -> Color -> Position -> [Position]
+freeMovements board turn position =
+  filter (\possibleDst -> not $ isCheck (placePiece board position possibleDst) turn) $
+    uncheckedFreeMovements
+      board
+      turn
+      position
+
+-- | Returns list of possible movements without considerations on King being in check
+uncheckedFreeMovements :: Board -> Color -> Position -> [Position]
+uncheckedFreeMovements board cc pos
+  | not (legalGrab board cc pos) = []
+  | isKnightOccupied board pos = _knightFreeMovements board cc pos
+  | isPawnOccupied board pos = _pawnFreeMovements board cc pos
+  | otherwise = _generalFreeMovements board cc pos
+
+-- | Retrieve list of free movements for a knight
+_knightFreeMovements :: Board -> Color -> Position -> [Position]
+_knightFreeMovements board turn position =
+  let isOccupiedByMe = isSquareOccupiedByColor turn . square board
+   in concatMap (filter (not . isOccupiedByMe)) (allMovements colouredPiece position)
+  where
+    selectedSquare = square board position
+    colouredPiece = piece selectedSquare
+
+_pawnFreeMovements :: Board -> Color -> Position -> [Position]
+_pawnFreeMovements board turn (x, y) =
+  let unboundedNormalPositions = takeWhile (isSquareFree board) $ concat $ allMovements pawnPiece (x, y)
+      unboundedCapturablePositions = filter (isPositionOccupiedByEnemy board turn) $ filter isInsideBoard [(x + forwardDir, y - 1), (x + forwardDir, y + 1)]
+   in unboundedNormalPositions ++ unboundedCapturablePositions
+  where
+    forwardDir = if isColor White pawnPiece then -1 else 1
+    selectedSquare = square board (x, y)
+    pawnPiece = piece selectedSquare
+
+_generalFreeMovements :: Board -> Color -> Position -> [Position]
+_generalFreeMovements board turn position = concatMap (availabilityPositionFilter board turn) (allMovements colouredPiece position)
+  where
+    selectedSquare = square board position
+    colouredPiece = piece selectedSquare
+
+pieceUnboundedMovements :: CPiece -> Position -> [[Position]]
+pieceUnboundedMovements (CPiece (Black, Pawn)) = bPawnMovements
+pieceUnboundedMovements (CPiece (White, Pawn)) = wPawnMovements
+pieceUnboundedMovements (CPiece (_, Rook)) = rookMovements
+pieceUnboundedMovements (CPiece (_, Knight)) = knightMovements
+pieceUnboundedMovements (CPiece (_, Bishop)) = bishopMovements
+pieceUnboundedMovements (CPiece (_, Queen)) = queenMovements
+pieceUnboundedMovements (CPiece (_, King)) = kingMovements
 
 -----------------------------------------------------------------------------------------
 -- LEGAL MOVES BY PIECE
